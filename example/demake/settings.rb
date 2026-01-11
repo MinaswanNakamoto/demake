7d159781ba69cecbcc2a7833f7f142bdd9660995
Minaswan Nakamoto <minaswan.nakamoto@proton.me>
2026-01-11 21:56:19 +0000

    Update settings.rb

--- example/demake/settings.rb
+++ example/demake/settings.rb
@@ -1,7 +1,15 @@
-STDERR.puts(@pipe.pipetext("|G|[check mark] |gdemake/settings.rb is present|n"))
-debug = false
-quiet = true
-compiler = "clang"
-flags = "-g -Wall"
-# No default flags
-$flags_override = true
+@silent = false
+#@compiler = "clang"
+#@emojis = false
+#@replace_with = "|b*|n"
+#@libraries << "-lc"
+#@flags << "-ansi -pedantic -D_DEFAULT_SOURCE"
+#@linux_flags << "-static"       # clang
+#@linux_flags << "-static -static-libgcc"
+#@x86_64_flags << "-msse2 -mssse3 -msse4.1 -msha"
+#@aarch64_flags << "-march=armv8-a+crypto"
+#@darwin_flags << "-bundle"
+#@flags = "-g -Wall"
+# No default flags (use only flags in the line above)
+#$flags_override = true
+notify("|g|[check mark] demake/settings.rb is present|n")
