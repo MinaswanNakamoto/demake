c7665651c3a3bf4c006e066e91cbb87b0306bd95
Minaswan Nakamoto <minaswan.nakamoto@proton.me>
2026-01-11 21:56:40 +0000

    Update test-target.rb

--- example/demake/test-target.rb
+++ example/demake/test-target.rb
@@ -1,4 +1,8 @@
 test_targets = <<-END_OF_STRING
-\t@echo "|R|[cross mark]|n  |rNo tests have been executed|n"
+\t@echo
+\t@echo "|YTesting command hello:|n"
+\tbin/hello
+\t@echo "|YTesting command goodbye:|n"
+\tbin/goodbye
 END_OF_STRING
 test_target << test_targets
