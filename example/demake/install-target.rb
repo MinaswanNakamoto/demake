ce64c4d265361c07806b33bf3cd634be8f6c7ec5
Minaswan Nakamoto <minaswan.nakamoto@proton.me>
2026-01-11 21:55:53 +0000

    Update install-target.rb

--- example/demake/install-target.rb
+++ example/demake/install-target.rb
@@ -1,4 +1,4 @@
 install_targets = <<-END_OF_STRING
-\t@echo "|R|[cross mark]|n  |rNo install scripts have been executed|n"
+\t@echo "#{@emoji_fail} |rInstation aborted -- no install options are provided for these applications|n"
 END_OF_STRING
 install_target << install_targets
