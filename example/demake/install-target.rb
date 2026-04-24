e138de94f24fbca116060de7caeac8add0b53d55
Minaswan Nakamoto <minaswan.nakamoto@proton.me>
2026-04-24 21:44:54 +0000

    Delete example directory

--- example/demake/install-target.rb
+++
@@ -1,4 +0,0 @@
-install_targets = <<-END_OF_STRING
-\t@echo "#{@emoji_fail} |rInstation aborted -- no install options are provided for these applications|n"
-END_OF_STRING
-install_target << install_targets
--- example/demake/test-target.rb
+++
@@ -1,8 +0,0 @@
-test_targets = <<-END_OF_STRING
-\t@echo
-\t@echo "|YTesting command hello:|n"
-\tbin/hello
-\t@echo "|YTesting command goodbye:|n"
-\tbin/goodbye
-END_OF_STRING
-test_target << test_targets
