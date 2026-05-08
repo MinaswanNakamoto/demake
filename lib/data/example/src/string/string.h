2598b040131c5ec67507eccbe5592ae5044789ae
Minaswan Nakamoto <minaswan.nakamoto@proton.me>
2026-05-08 17:36:55 +0000

    Delete lib/data directory

--- lib/data/example/src/string/string.h
+++
@@ -1,7 +0,0 @@
-/*
-   The most basic multiple executable example using a shared object/header file.
-
-   string.h includes a simple shared function named out which prints output
-*/
-
-void out(const char *in);
