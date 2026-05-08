c414fc95f2dd5879d15dc3379992a9f2e13de6a7
Minaswan Nakamoto <minaswan.nakamoto@proton.me>
2026-05-08 17:28:13 +0000

    Create string.h

---
+++ lib/apps/example/src/string/string.h
@@ -0,0 +1,7 @@
+/*
+   The most basic multiple executable example using a shared object/header file.
+
+   string.h includes a simple shared function named out which prints output
+*/
+
+void out(const char *in);
