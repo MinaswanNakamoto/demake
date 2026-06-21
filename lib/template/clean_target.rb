7338f4c80dbf816babc11a279a91fa275bc7e478
Minaswan Nakamoto <minaswan.nakamoto@proton.me>
2026-06-21 15:41:42 +0000

    Update clean_target.rb

--- lib/template/clean_target.rb
+++ lib/template/clean_target.rb
@@ -1,6 +1,30 @@
-@clean_target = <<-END_OF_STRING
+# defaults to only removing the object directory
+if(@clean_includes_binary_directory == true && @clean_includes_library_directory == true)
+  @clean_target = <<-END_OF_STRING
 .PHONY : clean
 clean :
 \t@echo "#{@emoji_clean} Removed |Yeverything|n from |R$(OBJECT)|n, |R$(BINARY)|n, and |R$(LIBRARY)|n directories"
 \t@rm -rf $(OBJECT) $(BINARY) $(LIBRARY)
 END_OF_STRING
+elsif(@clean_includes_binary_directory == true)
+  @clean_target = <<-END_OF_STRING
+.PHONY : clean
+clean :
+\t@echo "#{@emoji_clean} Removed |Yeverything|n from |R$(OBJECT)|n and |R$(BINARY)|n directories"
+\t@rm -rf $(OBJECT) $(BINARY) $(LIBRARY)
+END_OF_STRING
+elsif(@clean_includes_library_directory == true)
+  @clean_target = <<-END_OF_STRING
+.PHONY : clean
+clean :
+\t@echo "#{@emoji_clean} Removed |Yeverything|n from |R$(OBJECT)|n and |R$(LIBRARY)|n directories"
+\t@rm -rf $(OBJECT) $(BINARY) $(LIBRARY)
+END_OF_STRING
+else
+  @clean_target = <<-END_OF_STRING
+.PHONY : clean
+clean :
+\t@echo "#{@emoji_clean} Removed |Yeverything|n from |R$(OBJECT)|n directory"
+\t@rm -rf $(OBJECT)
+END_OF_STRING
+end
