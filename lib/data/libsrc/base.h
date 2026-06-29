557fc48f24778a9932e066faf0a757969e866576
Minaswan Nakamoto <minaswan.nakamoto@proton.me>
2026-06-29 20:32:07 +0000

    Create base.h

---
+++ lib/data/libsrc/base.h
@@ -0,0 +1,10 @@
+/*
+   base.h -- Light weight C Environment Setup
+*/
+
+#ifndef BASE_H_Minaswan /* Header guard to prevent multiple inclusions */
+  #define BASE_H_Minaswan
+  #define BASE_VERSION          "0.1.0"
+  #include "defines.h"
+  #include "typedefs.h"
+#endif /* BASE_H_Minaswan */
